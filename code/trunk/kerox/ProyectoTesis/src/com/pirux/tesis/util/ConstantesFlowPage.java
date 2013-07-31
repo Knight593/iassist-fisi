fd9fb4e35384f1be8578bc7e0906ca489406cf85
dmoises.cm@gmail.com <dmoises.cm@gmail.com@f264a502-7b88-5aaf-2441-2c9a79081f2e>
2013-07-31 05:05:57 +0000

    Se comitea la pagina de presentacion de Kerox : queda mucho por mejorar

--- code/trunk/kerox/ProyectoTesis/src/com/pirux/tesis/util/ConstantesFlowPage.java
+++ code/trunk/kerox/ProyectoTesis/src/com/pirux/tesis/util/ConstantesFlowPage.java
@@ -5,4 +5,5 @@ public class ConstantesFlowPage {
 	public static final String GO_PAGINA_TUTOR_HOME = "goTutorHome";
 	public static final String GO_PAGINA_HOME = "goHome";
 	public static final String GO_PAGINA_PROYECTOS_REALIZADOS = "goProyectosRealizados";
+	public static final String GO_PAGINA_PRESENTACION = "goPresentacion";
 }
